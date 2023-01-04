949bce72fe6897e2100ef3b65ee00690712ab540
TunahanAydinoglu <tunahan.aydinoglu@deliveryhero.com>
2023-01-04 18:48:46 +0300

    Initial Commit

---
+++ uikitexample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikitexample
+//
+//  Created by Tunahan Aydınoğlu on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
